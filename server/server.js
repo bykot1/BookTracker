0ee7dc0d5dae2bbbb96dc9a6f8667b5a10de4f5e
Trevor Byko <trevordbyko@gmail.com>
2025-02-03 22:52:28 -0700

    Cleaned up server.js file, added book genre data

--- server/server.js
+++ server/server.js
@@ -3,29 +3,29 @@ const express = require('express');
 const fs = require('fs')
 const path = require('path');
 const exphbs = require('express-handlebars');
-const PORT = process.env.PORT || 3000;
 // const connectDB = require('./config/db.js');
 // const bookRoutes = require('./routes/bookRoutes.js');
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 //Set up handlebars engine
-app.set('views', path.join(__dirname, 'views'));
 app.engine('hbs', exphbs.engine({ 
   extname: 'hbs', 
   defaultLayout: "main",
   partialsDir: path.join(__dirname, 'partials')
 }));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 //MongoDB
 // connectDB();
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../client')));
+// app.use(express.static(path.join(__dirname, '../client')));
 // app.use('/api/books', bookRoutes);
 
-//Handlebars templated serving
+//Handlebars routes
 app.get('/', (req, res) => {
   res.render('index');
 });
